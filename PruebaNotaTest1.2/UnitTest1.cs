e9d09e1ae1c9d8ab834b8ebaaae22d2e2db0918b
CNDV2021 <dielma21350@iesmarenostrum.com>
2021-06-26 17:36:14 +0200

    Falta mejorar los getter, setters y documentar el código.

--- PruebaNotaTest1.2/UnitTest1.cs
+++ PruebaNotaTest1.2/UnitTest1.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using PruebaNotas1._2;
 
 namespace PruebaNotaTest1._2
 {
@@ -20,11 +21,79 @@ namespace PruebaNotaTest1._2
             notas.Add(4);
             notas.Add(8);
 
-            double mediaesperada = 5.143;
-            int susE = 3;
-            int aprE = 1;
-            int notE = 2;
-            int sbrE = 1;
+            /*double mediaesperada = 5.143;
+            int suspensoEsperado = 3;
+            int aprobadoEsperado = 1;
+            int notableEsperado = 2;
+            int sobresalienteEsperado = 1;
+            */
         }
+
+        [TestMethod]
+        public void Test1()
+        {
+            int listaNota = 0;
+            int suspensoEsperado = 3;
+            _ = new List<int>();
+
+            try
+            {
+                listaNota.CompareTo(suspensoEsperado);
+            }
+
+            catch
+            {
+                throw new ArgumentOutOfRangeException("Error, la lista no contiene elementos");
+            }
+        }
+
+
+        [TestMethod]
+        public void Test2()
+        {
+            int suspensoActual = 3;
+            int suspensoEsperado = 3;
+            _ = new EstadisticasNotas();
+
+            Assert.AreEqual(suspensoActual, suspensoEsperado);
+        }
+
+
+        [TestMethod]
+        public void Test3()
+        {
+            int aprobadoActual = 4;
+            int aprobadoEsperado = 1;
+            _ = new EstadisticasNotas();
+
+            Assert.AreNotEqual(aprobadoActual, aprobadoEsperado);
+        }
+
+
+
+        [TestMethod]
+        public void Test4()
+        {
+            int notableActual = 2;
+            int notableEsperado = 2;
+            _ = new EstadisticasNotas();
+
+            Assert.AreEqual(notableActual, notableEsperado);
+
+        }
+
+
+        [TestMethod]
+        public void Test5()
+        {
+            int sobresalienteActual = 5;
+            int sobresalienteEsperado = 1;
+            _ = new EstadisticasNotas();
+
+            Assert.AreNotEqual(sobresalienteActual, sobresalienteEsperado);
+            
+        }
+
+
     }
 }
--- PruebaNotas1.2/Class1.cs
+++ PruebaNotas1.2/Class1.cs
@@ -6,38 +6,76 @@ using System.Threading.Tasks;
 
 namespace PruebaNotas1._2
 {
-    public class estNot  // esta clase nos calcula las estadísticas de un conjunto de notas
+    public class EstadisticasNotas  // esta clase nos calcula las estadísticas de un conjunto de notas
     {
-        public int sus;  // Suspensos
-        public int apr;  // Aprobados
-        public int no;  // Notables
-        public int sbr;  // Sobresalientes
+        private int suspensos;  // Suspensos
+        private int aprobados;  // Aprobados
+        private int notables;  // Notables
+        private int sobresalientes;  // Sobresalientes
 
-        public double media;  // Nota media
+        private double media;  // Nota media
+
+        public int Suspensos
+        {
+            get => suspensos;
+            set => suspensos = value;
+        }
+        public int Aprobados
+        {
+            get => aprobados;
+            set => aprobados = value;
+        }
+        public int Notables
+        {
+            get => notables;
+            set => notables = value;
+        }
+        public int Sobresalientes
+        {
+            get => sobresalientes;
+            set => sobresalientes = value;
+        }
+        public double Media
+        {
+            get => media;
+            set => media = value;
+        }
 
         // Constructor vacío
-        public estNot()
+        public EstadisticasNotas()
         {
-            sus = apr = no = sbr = 0;  // inicializamos las variables
-            media = 0.0;
+            Suspensos = Aprobados = Notables = Sobresalientes = 0;  // inicializamos las variables
+            Media = 0.0;
         }
 
         // Constructor a partir de un array, calcula las estadísticas al crear el objeto
-        public estNot(List<int> listnot)
+        public EstadisticasNotas(List<int> listaNota)
         {
-            media = 0.0;
+            Media = 0.0;
 
-            for (int i = 0; i < listnot.Count; i++)
+            for (int i = 0; i < listaNota.Count; i++)
             {
-                if (listnot[i] < 5) sus++;  // Por debajo de 5 suspenso
-                else if (listnot[i] > 5 && listnot[i] < 7) apr++;  // Nota para aprobar: 5
-                else if (listnot[i] > 7 && listnot[i] < 9) no++;  // Nota para notable: 7
-                else if (listnot[i] > 9) sbr++;  // Nota para sobresaliente: 9
+                if (listaNota[i] < 5)
+                {
+                    Suspensos++;  // Por debajo de 5 suspenso
+                }
+                else if (listaNota[i] > 5 && listaNota[i] < 7)
+                {
+                    Aprobados++;  // Nota para aprobar: 5
+                }
+                else if (listaNota[i] > 7 && listaNota[i] < 9)
+                {
+                    Notables++;  // Nota para notable: 7
+                }
+                else if (listaNota[i] > 9)
+                {
+                    Sobresalientes++;  // Nota para sobresaliente: 9
+                }
 
-                media = media + listnot[i];
+                Media += listaNota[i];
             }
 
-            media = media / listnot.Count;
+            Media /= listaNota.Count;
         }
 
 
@@ -45,32 +83,52 @@ namespace PruebaNotas1._2
         // calcula la media y el número de suspensos/aprobados/notables/sobresalientes
         //
         // El método devuelve -1 si ha habido algún problema, la media en caso contrario	
-        public double calcEst(List<int> listnot)
+        public double CalcularEstadisticas(List<int> listaNota)
         {
-            media = 0.0;
+            Media = 0.0;
 
-            // TODO: hay que modificar el tratamiento de errores para generar excepciones
+            //x TODO: hay que modificar el tratamiento de errores para generar excepciones
             //
-            if (listnot.Count <= 0)  // Si la lista no contiene elementos, devolvemos un error
-                return -1;
-
-            for (int i = 0; i < 10; i++)
-                if (listnot[i] < 0 || listnot[i] > 10)  // comprobamos que las not están entre 0 y 10 (mínimo y máximo), si no, error
-                    return -1;
-
-            for (int i = 0; i < listnot.Count; i++)
+            if (listaNota.Count <= 0)  // Si la lista no contiene elementos, devolvemos un error
             {
-                if (listnot[i] < 5) sus++;  // Por debajo de 5 suspenso
-                else if (listnot[i] >= 5 && listnot[i] < 7) apr++;  // Nota para aprobar: 5
-                else if (listnot[i] >= 7 && listnot[i] < 9) no++;  // Nota para notable: 7
-                else if (listnot[i] > 9) sbr++;  // Nota para sobresaliente: 9
-
-                media = media + listnot[i];
+                throw new ArgumentOutOfRangeException("Error, la lista no contiene elementos");
             }
 
-            media = media / listnot.Count;
+            //return -1;
 
-            return media;
+            for (int i = 0; i < 10; i++)
+                if (listaNota[i] < 0 || listaNota[i] > 10)  // comprobamos que las not están entre 0 y 10 (mínimo y máximo), si no, error
+                {
+                    throw new ArgumentOutOfRangeException("Error, Las notas no están entre el mínimo y máximo");
+                }
+
+            //return -1;
+
+            for (int i = 0; i < listaNota.Count; i++)
+            {
+                if (listaNota[i] < 5)
+                {
+                    Suspensos++;  // Por debajo de 5 suspenso
+                }
+                else if (listaNota[i] >= 5 && listaNota[i] < 7)
+                {
+                    Aprobados++;  // Nota para aprobar: 5
+                }
+                else if (listaNota[i] >= 7 && listaNota[i] < 9)
+                {
+                    Notables++;  // Nota para notable: 7
+                }
+                else if (listaNota[i] > 9)
+                {
+                    Sobresalientes++;  // Nota para sobresaliente: 9
+                }
+
+                Media += listaNota[i];
+            }
+
+            Media /= listaNota.Count;
+
+            return Media;
         }
     }
 }
